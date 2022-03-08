164fbcfa724b633204451e1a07f772a1b67599bf
Kayllane <pinakayllane@gmail.com>
2022-03-08 11:27:35 -0300

    Primeiros commits

---
+++ src/br/com/segundoprojeto/Aplicacao.java
@@ -0,0 +1,12 @@
+package br.com.segundoprojeto;
+
+import java.util.List;
+
+public class Aplicacao{
+
+    private List<LeituraDeArquivos> leituraDeArquivos;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/br/com/segundoprojeto/LeituraDeArquivos.java
@@ -0,0 +1,18 @@
+package br.com.segundoprojeto;
+
+public class LeituraDeArquivos {
+    Integer index;
+    Integer year;
+    Integer age;
+    String name;
+    String movie;
+
+    public LeituraDeArquivos(Integer index, Integer year, Integer age, String name, String movie){
+        this.index = index;
+        this.year = year;
+        this.age = age;
+        this.name = name;
+        this.movie = movie;
+    }
+
+}
